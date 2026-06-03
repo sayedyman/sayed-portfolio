1ef034ffc6a23d4bc3bc239edbea3a0c4563f1aa
Sayed Ayman Elghanam <sayedelghanam123@gmail.com>
2026-06-03 23:17:11 +0300

    feat: refine projects showcase and favicon system

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -10,17 +10,5 @@ export default function manifest(): MetadataRoute.Manifest {
     display: 'standalone',
     background_color: '#050505',
     theme_color: '#050505',
-    icons: [
-      {
-        src: '/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
   }
 }
